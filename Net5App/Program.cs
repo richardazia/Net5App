859c0e6ae1b74a4d010e6afd72c7291d6eecba61
Richard A <richard@main-vision.com>
2022-03-22 10:31:52 +0100

    add Good Morning Richard keep window open

--- Net5App/Program.cs
+++ Net5App/Program.cs
@@ -4,9 +4,13 @@ namespace Net5App
 {
     internal class Program
     {
+        //Entry point of program
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Good Morning Richard!");
+            // Keep the console open with: 
+            Console.Read();
         }
     }
 }
